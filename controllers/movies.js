0ec9ad2b4111730f6f227ad963baa5bb4c12883a
nicholaskier <nicholaskier@gmail.com>
2020-12-11 19:26:33 -0600

    add directories and touch files as well as API call URL

---
+++ controllers/movies.js
@@ -0,0 +1,5 @@
+// https://api.themoviedb.org/3/discover/movie?api_key=128f779ecf9c9c11602257711da1a19f&language=en-US&region=US&sort_by=vote_count.asc&include_adult=false&include_video=false&page=1&vote_count.gte=10&vote_average.lte=5.5&with_runtime.gte=60&with_original_language=en
+
+// correct URL for API call 
+// 110 pages
+// 2195 results
